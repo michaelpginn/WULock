a47736a9d834bc64a949241e723dd3ba107d2900
Michael Ginn <milespergallon271@gmail.com>
2018-11-28 12:40:15 -0600

    added todo list

---
+++ WULock/TODO LIST.swift
@@ -0,0 +1,40 @@
+//
+//  TODO LIST.swift
+//  WULock
+//
+//  Created by Michael Ginn on 11/28/18.
+//  Copyright © 2018 Michael Ginn. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ TODO LIST
+ Just a list of stuff we either need to or could do. Feel free to add.
+ 
+ - AR Tools:
+    ( ) get good reference images
+    ( ) let user select combinations to use
+    ( ) display instructions
+    ( ) allow instruction navigation
+ - Vault:
+    ( ) show detail view for item when tapped
+    ( ) for mail combo and gym combo, enforce rules for entry
+    ( ) sorting and filtering
+    ( ) display "No items message"
+    ( ) don't cover fields with keyboard, slide table up
+    ( ) delete/edit items
+    ( ) let online items link to browser
+ - ID Scanner:
+    ( ) display bigger version when card is tapped
+    ( ) allow replacement of image (could be same view)
+    ( ) display overlay while taking picture so user knows what rectangle is being selected
+    ( ) maybe link with vault? so id type record could have image?
+ - General/Misc:
+    ( ) touch id or passcode to lock vault
+    ( ) hide passwords
+    ( ) maybe implement Password AutoFill API? https://developer.apple.com/documentation/security/password_autofill/
+    ( ) sync vault with icloud (this is a last priority)
+ */
+
